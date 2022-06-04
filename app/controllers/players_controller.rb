65caa3b744d6342b687bb999ee4a0f4707fb3a87
Gabriel Fuertes <gabifuertes@gmail.com>
2022-06-04 13:38:39 -0300

    ✨ Implement sign up methods and routes

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,27 @@
 class PlayersController < ApplicationController
+  
+  def new
+    @player = Player.new
+  end
+  
+  def create
+    @player = Player.new(player_params)
+    @player.email.downcase!
+    if @player.save
+      redirect_to login
+    else
+      # TODO
+      # Handle error creating account.
+      # flash.now.alert = "Couldn't create account."
+      # render :new
+    end
+  end
+
+  # TODO
+  # Add methods for modifying and destroying accounts.
+
+  private
+    def player_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Sign up form
+  get 'signup', to: 'players#new', as: 'player'
+  # Sign up
+  post 'signup', to: 'players#create'
 end

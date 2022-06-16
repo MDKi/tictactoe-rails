33352e8b861ef12bf7cc9c0775262a5fdc41f1e5
Gabriel Fuertes <gabifuertes@gmail.com>
2022-06-16 20:32:37 -0300

    ✨ Add session routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,15 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  # Sign up form
+  # Sign up / Register form
   get 'signup', to: 'players#new', as: 'players'
-  # Sign up
+  # Sign up / Register
   post 'signup', to: 'players#create'
+
+  # Sign in / Log in form
+	get 'signin', to: 'sessions#new'
+	# Sign in / Log in
+	post 'signin', to: 'sessions#create'
+	# Sign out / Log out
+	delete 'signout', to: 'sessions#destroy'  
 end
